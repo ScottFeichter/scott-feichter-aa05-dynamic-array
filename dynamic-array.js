d1b861a24d92384f24bb3579b51542a402741e3d
Timothy <tim.3imt@gmail.com>
2024-02-06 17:54:13 -0600

    finished dynamic array

--- dynamic-array.js
+++ dynamic-array.js
@@ -2,43 +2,86 @@ class DynamicArray {
 
   constructor(defaultSize = 4) {
 
-    // Your code here 
+    // Your code here
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
 
-    // Your code here 
+    // Your code here
+    return this.data[index];
   }
 
   push(val) {
 
-    // Your code here 
+    // Your code here
+    if (this.length === this.capacity) this.resize();
+
+    this.data[this.length] = val;
+    return ++this.length;
   }
 
 
   pop() {
 
-    // Your code here 
-  }
-
-  shift() {
-
-    // Your code here 
+    // Your code here
+    if (this.length) {
+      let temp = JSON.parse(JSON.stringify(this.data[this.length - 1]));
+      this.data[this.length - 1] = undefined;
+      this.length--;
+      console.log(temp);
+      return temp;
+    } else {
+      return undefined;
+    }
   }
 
   unshift(val) {
 
-    // Your code here 
+    // Your code here
+    if (this.length === this.capacity) this.resize();
+
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    return ++this.length;
+  }
+
+  shift() {
+
+    // Your code here
+    if (this.length) {
+      let temp = this.data[0];
+      for (let i = 0; i < this.length - 1; i++) {
+        this.data[i] = this.data[i + 1];
+      }
+      this.data[this.length - 1] = undefined;
+      this.length--;
+      return temp;
+    } else {
+      return undefined;
+    }
   }
 
   indexOf(val) {
 
-    // Your code here 
+    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) return i;
+    }
+    return -1;
   }
 
   resize() {
 
-    // Your code here 
+    // Your code here
+    this.capacity *= 2;
+    for (let i = this.length; i < this.capacity; i++) {
+      this.data[i] = undefined;
+    }
   }
 
 }
